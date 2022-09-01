c8b5b6b8ec2780c17702aaa14336e4888c257c8b
ChonchitoFeliz <jesusandresrodriguezgarcia@outlook.com>
2022-09-01 10:38:56 -0500

    Initial commit

---
+++ app/src/main/java/com/innova/parcial/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.innova.parcial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        Leer los valores de dos ángulos A y B, con un menú con las siguientes opciones:
+//
+//        Mostar las Raíces Cuadradas y cúbicas de ambos ángulos
+//        Mostrar los valores de las funciones Seno, Coseno, Tangente y Cotangente
+//        Mostrar  A elevado a la  B
+//                Todos los valores de repuesta debe ser presentados como números quebrados, por ejemplo 0.5 debe mostrarse como 1/2  y 3,141592 como 333/106
+//
+//        La tolerancia, es decir la diferencia absoluta entre 3.141592 y 333/106 debe ser como mínima de 0.01
+//
+//        Nota: Probar códigos https://replit.com/
+//
+//        Entregar:
+//
+//        Carpeta de proyecto comprimida
+//                Informe de app (Documento en word describe el funcionamiento con pantallazos)
+
+
+
+    }
+}
