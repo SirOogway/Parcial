ed7ee600d5206f6b22d4074e2ebfa1532230f2af
ChonchitoFeliz <jesusandresrodriguezgarcia@outlook.com>
2022-09-05 02:21:40 -0500

    it only remains to represent results as broken

--- app/src/main/java/com/innova/parcial/MainActivity.kt
+++ app/src/main/java/com/innova/parcial/MainActivity.kt
@@ -3,6 +3,7 @@ package com.innova.parcial
 import android.content.res.ColorStateList
 import android.os.Bundle
 import android.text.InputType
+import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
@@ -11,8 +12,10 @@ import androidx.core.widget.doAfterTextChanged
 import androidx.core.widget.doBeforeTextChanged
 import androidx.core.widget.doOnTextChanged
 import com.innova.parcial.databinding.ActivityMainBinding
+import java.lang.Exception
 import java.lang.Math.*
 import kotlin.math.cos
+import kotlin.math.pow
 import kotlin.math.sin
 import kotlin.math.sqrt
 
@@ -25,8 +28,6 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
         val view = binding.root
         setContentView(view)
 
-        // CREAR LOGICA PARA CUANDO HAYA UN EDIT TEXT VACIO Y SE PRECIONE EL BOTON NO SE CIERRE LA APP
-
         val operations = resources.getStringArray(R.array.operations)
         val adapter = ArrayAdapter(this, R.layout.list_item, operations)
         binding.dropDownMenu.apply {
@@ -36,9 +37,9 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
         }
 //        Leer los valores de dos ángulos A y B, con un menú con las siguientes opciones:
 //
-//       * Mostar las Raíces Cuadradas y cúbicas de ambos ángulos
-//       * Mostrar los valores de las funciones Seno, Coseno, Tangente y Cotangente
-//       * Mostrar  A elevado a la  B
+//       * Mostar las Raíces Cuadradas y cúbicas de ambos ángulos :D
+//       * Mostrar los valores de las funciones Seno, Coseno, Tangente y Cotangente :D
+//       * Mostrar  A elevado a la  B :D
 //                Todos los valores de repuesta debe ser presentados como números quebrados, por ejemplo 0.5 debe mostrarse como 1/2  y 3,141592 como 333/106
 //
 //        La tolerancia, es decir la diferencia absoluta entre 3.141592 y 333/106 debe ser como mínima de 0.01
@@ -61,24 +62,22 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
                 firstValidation = etAngleA.text.toString() != ""
                 if (!firstValidation || !secondValidation) disableRadioButtons(myRadioButtonList)
                 else activateRadioButtons(myRadioButtonList)
+
+                etAngleA.error = if (!isFilledField(etAngleA)) "Enter a number" else null
             }
 
             etAngleB.doAfterTextChanged {
                 secondValidation = etAngleB.text.toString() != ""
                 if (!firstValidation || !secondValidation) disableRadioButtons(myRadioButtonList)
                 else activateRadioButtons(myRadioButtonList)
+
+                etAngleB.error = if (!isFilledField(etAngleB)) "Enter a number" else null
             }
         }
     }
 
-    //tal vez sean radianes probar LUEGOOOOOOOOOOOOOOOOOOOOOOOO
-//    fun calculateSine(angle: Double) = sin(angle)
-//
-//    fun calculateCosine(angle: Double) = cos(angle * Math.PI / 180)
-//
-//    fun calculateTangent(angle: Double) = kotlin.math.tan(angle * Math.PI / 180)
+    private fun isFilledField(editText: EditText) = editText.text.toString() != ""
 
-    //
     override fun onItemClick(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
         binding.apply {
             val myRadioGroupList = listOf(rgRoot, rgTrigonoRelations)
@@ -87,53 +86,81 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
                 "Roots" -> {
                     defineVisibilityToRadioGroup(rgRoot, myRadioGroupList)
                     btnCalculate.setOnClickListener {
-                        val angleA: Double = etAngleA.text.toString().toDouble()
-                        val angleB: Double = etAngleB.text.toString().toDouble()
+                        try {
+                            val angleA: Double = etAngleA.text.toString().toDouble()
+                            val angleB: Double = etAngleB.text.toString().toDouble()
 
-                        tvResult.text = when {
-                            rbSquare.isChecked -> "Root A: ${sqrt(angleA)} \nRoot B: ${sqrt(angleB)}"
-                            rbCubic.isChecked -> "Root A: ${cbrt(angleA)} \nRoot B: ${cbrt(angleB)}"
-                            else -> {
-                                Toast.makeText(
-                                    this@MainActivity,
-                                    "Choose a option",
-                                    Toast.LENGTH_SHORT
-                                )
-                                    .show()
-                                ""
+                            tvResult.text = when {
+                                rbSquare.isChecked -> "Root A: ${sqrt(angleA)} \nRoot B: ${
+                                    sqrt(
+                                        angleB
+                                    )
+                                }"
+                                rbCubic.isChecked -> "Root A: ${cbrt(angleA)} \nRoot B: ${
+                                    cbrt(
+                                        angleB
+                                    )
+                                }"
+                                else -> {
+                                    Toast.makeText(
+                                        this@MainActivity,
+                                        "Choose a option",
+                                        Toast.LENGTH_SHORT
+                                    )
+                                        .show()
+                                    ""
+                                }
                             }
+                        } catch (e: Exception) {
                         }
                     }
                 }
                 "Trigonometric relations" -> {
                     defineVisibilityToRadioGroup(rgTrigonoRelations, myRadioGroupList)
                     btnCalculate.setOnClickListener {
-                        val angleA: Double = etAngleA.text.toString().toDouble()
-                        val angleB: Double = etAngleB.text.toString().toDouble()
+                        try {
+                            val angleA: Double = etAngleA.text.toString().toDouble()
+                            val angleB: Double = etAngleB.text.toString().toDouble()
 
-                        tvResult.text = when {
-                            rbSine.isChecked -> "Sine A: ${sin(angleA)} \nSine B ${sin(angleB)}"
-                            rbCosine.isChecked -> "Cosine A: ${cos(angleA)} \nCosine B ${cos(angleB)}"
-                            rbTangent.isChecked -> "Tangent A: ${kotlin.math.tan(angleA)} \nTangent B: ${
-                                kotlin.math.tan(angleB)
-                            }"
-                            rbCotangent.isChecked -> "Cotangent A: ${cos(angleA) / sin(angleA)} \nCotangent B: ${
-                                cos(angleB) / sin(angleB)
-                            }"
-                            else -> {
-                                Toast.makeText(
-                                    this@MainActivity,
-                                    "Choose a option",
-                                    Toast.LENGTH_SHORT
-                                )
-                                    .show()
-                                ""
+                            tvResult.text = when {
+                                rbSine.isChecked -> "Sine A: ${sin(angleA)} \nSine B ${sin(angleB)}"
+                                rbCosine.isChecked -> "Cosine A: ${cos(angleA)} \nCosine B ${
+                                    cos(
+                                        angleB
+                                    )
+                                }"
+                                rbTangent.isChecked -> "Tangent A: ${kotlin.math.tan(angleA)} \nTangent B: ${
+                                    kotlin.math.tan(angleB)
+                                }"
+                                rbCotangent.isChecked -> "Cotangent A: ${cos(angleA) / sin(angleA)} \nCotangent B: ${
+                                    cos(angleB) / sin(angleB)
+                                }"
+                                else -> {
+                                    Toast.makeText(
+                                        this@MainActivity,
+                                        "Choose a option",
+                                        Toast.LENGTH_SHORT
+                                    )
+                                        .show()
+                                    ""
+                                }
                             }
+                        } catch (e: Exception) {
                         }
                     }
                 }
                 "Raise" -> {
                     defineVisibilityToRadioGroup(null, myRadioGroupList)
+                    btnCalculate.setOnClickListener {
+                        try {
+                            val angleA: Double = etAngleA.text.toString().toDouble()
+                            val angleB: Double = etAngleB.text.toString().toDouble()
+
+                            tvResult.text = angleA.pow(angleB).toString()
+                        } catch (e: NumberFormatException) {
+                            Log.d("Error", "Este es mi error${e}")
+                        }
+                    }
                 }
             }
         }
